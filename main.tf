affdc71a7a29a756886bc59ee0b8da9d26bc63c1
sushma-sm <gurajasushmamounika@gmail.com>
2025-04-23 21:37:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,37 @@
+terraform {
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.12.1"
+    }
+  }
+
+  required_version = ">= 1.3.0"
+}
+
 provider "helm" {
   kubernetes {
     config_path = "~/.kube/config"
   }
 }
 
-module "delegate" {
-  source          = "harness/harness-delegate/kubernetes"
-  version         = "0.1.8"
+resource "kubernetes_namespace" "delegate_ns" {
+  metadata {
+    name = "harness-delegate-ng"
+  }
+}
 
-  account_id      = "ucHySz2jQKKWQweZdXyCog"
-  delegate_token  = "NTRhYTY0Mjg3NThkNjBiNjMzNzhjOGQyNjEwOTQyZjY="
-  delegate_name   = "terraform-delegate"
-  deploy_mode     = "KUBERNETES"
-  namespace       = "harness-delegate-ng"
-  manager_endpoint = "https://app.harness.io"
-  delegate_image  = "us-docker.pkg.dev/gar-prod-setup/harness-public/harness/delegate:25.04.85602"
-  replicas        = 1
-  upgrader_enabled = true
+module "delegate" {
+  source  = "harness/harness-delegate/kubernetes"
+  version = "0.1.8"
+
+  account_id        = var.harness_account_id
+  delegate_token    = var.delegate_token
+  delegate_name     = "terraform-delegate"
+  deploy_mode       = "KUBERNETES"
+  namespace         = kubernetes_namespace.delegate_ns.metadata[0].name
+  manager_endpoint  = "https://app.harness.io"
+  delegate_image    = "us-docker.pkg.dev/gar-prod-setup/harness-public/harness/delegate:25.04.85602"
+  replicas          = 1
+  upgrader_enabled  = true
 }
