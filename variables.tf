c805155731c52e1bd4e239297f1cb72ba4233a31
sushma-sm <gurajasushmamounika@gmail.com>
2025-04-23 21:38:48 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "harness_account_id" {
+  type        = string
+  description = "Harness account ID"
+}
+
+variable "delegate_token" {
+  type        = string
+  description = "Harness delegate token"
+}
