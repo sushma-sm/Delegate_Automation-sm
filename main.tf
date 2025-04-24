4385b5fd5637fd5a7221e7a209d31f20fccbe29c
sushma-sm <gurajasushmamounika@gmail.com>
2025-04-24 17:55:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,6 @@
 resource "google_container_cluster" "primary" {
   name     = var.cluster_name
   location = var.region
-
-  deletion_protection = false
-
   initial_node_count = var.node_count
 
   node_config {
