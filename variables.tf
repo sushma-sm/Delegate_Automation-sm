7b84111cea5dc3327fd857f0ba9f7c236503441a
sushma-sm <gurajasushmamounika@gmail.com>
2025-04-24 16:13:23 +0530

    Delete variables.tf

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "harness_account_id" {
-  type        = string
-  description = "Harness account ID"
-}
-
-variable "delegate_token" {
-  type        = string
-  description = "Harness delegate token"
-}
